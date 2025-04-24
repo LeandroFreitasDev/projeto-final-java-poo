c8178499c535f50f206837d0aec5743b95f59156
PFreiitaz <pedrodefre88@gmail.com>
2025-04-24 14:43:29 -0300

    Atualizações main

--- src/org/serratec/main/Main.java
+++ src/org/serratec/main/Main.java
@@ -1,10 +1,18 @@
 package org.serratec.main;
 
+import java.util.List;
+
+import org.serratec.projeto.LeituraGravacaoArquivo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LeituraGravacaoArquivo a = new LeituraGravacaoArquivo();
+		List<String[]> dadosPorLinha = a.lerArquivo();
+		if (!dadosPorLinha.isEmpty()) {
+			a.processarArquivo(dadosPorLinha);
+			a.gerarCPFDuplicados();
+			a.gerarFolha();
+		}
 	}
-
 }
